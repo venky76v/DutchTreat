a901d565b898f924ef3b97dcbe9b35b393308741
Venky Venkataraman <venky76v@gmail.com>
2017-11-14 22:34:58 +0000

    Add project files.

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            throw new InvalidOperationException("Bad things happen");
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+
+        }
+    }
+}
